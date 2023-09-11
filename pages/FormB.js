01e5ed896e9ba6dd993fcd43a1865a8783ebacfc
Vishal Sharma <785vishal443@gmail.com>
2023-09-11 23:18:01 +0530

    Update FormB.js

--- pages/FormB.js
+++ pages/FormB.js
@@ -1,32 +1,39 @@
-'use client';
-import { useState, useEffect } from 'react';
-function FormB({ onSubmit, age }) {
-  const [marvelShows, setMarvelShows] = useState('');
+"use client";
+import { useState, useEffect } from "react";
+function FormB({ onSubmit, age, setFormData, formData }) {
+  const [marvelShows, setMarvelShows] = useState("");
 
-  const handleSubmit = (event) => {};
+  const handleSubmit = (event) => {
+    onSubmit("Form B");
+  };
 
   return (
-    <form id='marvel'>
+    <form id="marvel" onSubmit={handleSubmit}>
       <h2>Form B</h2>
       <label>
         Select Marvel Shows:
-        <select>
-          <option value=''>--Select--</option>
-          <option value='WandaVision'>WandaVision</option>
-          <option value='The Falcon and the Winter Soldier'>
+        <select
+          value={formData?.["Marvel Shows"]}
+          onChange={(e) =>
+            setFormData({ ...formData, "Marvel Shows": e.target.value })
+          }
+        >
+          <option value="">--Select--</option>
+          <option value="WandaVision">WandaVision</option>
+          <option value="The Falcon and the Winter Soldier">
             The Falcon and the Winter Soldier
           </option>
-          <option value='Loki'>Loki</option>
-          <option value='What If...?'>What If...?</option>
+          <option value="Loki">Loki</option>
+          <option value="What If...?">What If...?</option>
         </select>
       </label>
       <br />
       <label>
         Enter your age:
-        <input type='number' value={age} disabled />
+        <input type="number" value={age} disabled />
       </label>
       <br />
-      <button id='submit-marvel' type='submit'>
+      <button id="submit-marvel" type="submit">
         Submit
       </button>
     </form>

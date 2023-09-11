52c800bc8d22cfa900c0cfc78a3f16b8dad0fecc
Vishal Sharma <785vishal443@gmail.com>
2023-09-11 23:17:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,42 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import FormA from './FormA';
-import FormB from './FormB';
-import Summary from './Summary';
+"use client";
+import { useState, useEffect } from "react";
+function FormB({ onSubmit, age, setFormData, formData }) {
+  const [marvelShows, setMarvelShows] = useState("");
 
-const App = () => {
-  const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState({});
-  const [age, setAge] = useState('');
+  const handleSubmit = (event) => {
+    onSubmit("Form B");
+  };
 
   return (
-    <div>
-      {(step === 1 || !age) && (
-        <div id='start-page'>
-          <h1>Step 1: Select Form Type and Enter Age</h1>
-          <label>
-            Enter your age:
-            <input value={age} />
-          </label>
-          <br />
-          <label>
-            Select Form Type:
-            <select onChange={(e) => setStep(parseInt(e.target.value))}>
-              <option value={1}>--Select--</option>
-              <option value={2}>Form A</option>
-              <option value={3}>Form B</option>
-            </select>
-          </label>
-          <br />
-        </div>
-      )}
-      {step === 2 && (
-        <div>
-          <FormA age={age} />
-        </div>
-      )}
-      {step === 3 && (
-        <div>
-          <FormB age={age} />
-        </div>
-      )}
-      {(step === 2 || step === 3) && age ? (
-        <button id='go-back' onClick={() => setStep(1)}>
-          Go Back
-        </button>
-      ) : null}
-
-      {step === 4 && (
-        <div>
-          <Summary />
-          <button id='start-over'>Start Over</button>
-        </div>
-      )}
-    </div>
+    <form id="marvel" onSubmit={handleSubmit}>
+      <h2>Form B</h2>
+      <label>
+        Select Marvel Shows:
+        <select
+          value={formData?.["Marvel Shows"]}
+          onChange={(e) =>
+            setFormData({ ...formData, "Marvel Shows": e.target.value })
+          }
+        >
+          <option value="">--Select--</option>
+          <option value="WandaVision">WandaVision</option>
+          <option value="The Falcon and the Winter Soldier">
+            The Falcon and the Winter Soldier
+          </option>
+          <option value="Loki">Loki</option>
+          <option value="What If...?">What If...?</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Enter your age:
+        <input type="number" value={age} disabled />
+      </label>
+      <br />
+      <button id="submit-marvel" type="submit">
+        Submit
+      </button>
+    </form>
   );
-};
-
-export default App;
+}
+export default FormB;

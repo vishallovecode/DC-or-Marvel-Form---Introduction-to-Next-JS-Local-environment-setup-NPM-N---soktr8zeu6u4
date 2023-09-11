c34e1f794abeec08c9d4fef7f334dddcbed804b8
Vishal Sharma <785vishal443@gmail.com>
2023-09-11 23:17:30 +0530

    Update FormA.js

--- pages/FormA.js
+++ pages/FormA.js
@@ -1,28 +1,35 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
-function FormA({ onSubmit, age }) {
-  const handleSubmit = (event) => {};
+function FormA({ onSubmit, age, setFormData, formData }) {
+  const handleSubmit = (event) => {
+    onSubmit("Form A");
+  };
   return (
-    <form id='dc' onSubmit={handleSubmit}>
+    <form id="dc" onSubmit={handleSubmit}>
       <h2>Form A</h2>
       <label>
         Select DC Shows:
-        <select>
-          <option value=''>--Select--</option>
-          <option value='The Flash'>The Flash</option>
-          <option value='Arrow'>Arrow</option>
-          <option value='Supergirl'>Supergirl</option>
-          <option value='Legends of Tomorrow'>Legends of Tomorrow</option>
+        <select
+          onChange={(e) =>
+            setFormData({ ...formData, "DC Shows": e.target.value })
+          }
+          value={formData?.["DC Shows"]}
+        >
+          <option value="">--Select--</option>
+          <option value="The Flash">The Flash</option>
+          <option value="Arrow">Arrow</option>
+          <option value="Supergirl">Supergirl</option>
+          <option value="Legends of Tomorrow">Legends of Tomorrow</option>
         </select>
       </label>
       <br />
       <label>
         Enter your age:
-        <input type='number' value={age} disabled />
+        <input type="number" value={age} disabled />
       </label>
       <br />
-      <button id='submit-dc' type='submit'>
+      <button id="submit-dc" type="submit">
         Submit
       </button>
     </form>
